133a77caf9d28dba32d955084a768e181d072105
Diogo Alves <the.diugu.92@gmail.com>
2022-08-17 01:07:10 +0100

    added migrations on starting up the app

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WeatherAPI.Data;
 
 namespace WeatherAPI
 {
@@ -13,7 +16,16 @@ namespace WeatherAPI
 	{
 		public static void Main(string[] args)
 		{
-			CreateHostBuilder(args).Build().Run();
+			//CreateHostBuilder(args).Build().MigrateDatabase().Run();
+			var host = CreateHostBuilder(args).Build();
+
+			using (var scope = host.Services.CreateScope())
+			{
+				var db = scope.ServiceProvider.GetRequiredService<WeatherDbContext>();
+				db.Database.Migrate();
+			}
+
+			host.Run();
 		}
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
